64b3ba871373e6ea6ba5d22a9b100d63dc7061f9
Kuroro Lucifer <isgrammar@gmail.com>
2021-05-28 20:03:32 +0300

    reg1

--- src/index.js
+++ src/index.js
@@ -1,17 +1,24 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import App from './components/App';
+import {BrowserRouter as Router, Route, Switch} from "react-router-dom";
+import Login from "./components/Auth/Login";
+import Register from "./components/Auth/Register";
+import 'semantic-ui-css/semantic.min.css'
+
+const Root = () => (
+    <Router>
+        <Switch>
+            <Route path='/' component={App} exact />
+            <Route path='/login' component={Login} />
+            <Route path='/register' component={Register} />
+        </Switch>
+    </Router>
+)
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+    <Root />,
   document.getElementById('root')
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
