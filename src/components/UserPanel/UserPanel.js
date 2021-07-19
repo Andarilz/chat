bfd4e585730f3b9a4443c354c618b9977f63ecd2
Andarilz <tartarus@gmail.com>
2021-07-19 16:00:30 +0300

    'end2'

--- src/components/UserPanel/UserPanel.js
+++ src/components/UserPanel/UserPanel.js
@@ -39,36 +39,6 @@ class UserPanel extends Component{
      }
 
 
-    // Listeners = async () => {
-    //
-    //     await axios.get(`https://chat-14c5a-default-rtdb.europe-west1.firebasedatabase.app/users.json`)
-    //         .then(res => {
-    //             const results = res.data || []
-    //
-    //             if (results) {
-    //                 const keysOfMessages = Object.keys(results) //получаем ключи объектов с сообщениями
-    //
-    //                 const mess = keysOfMessages.map(res => results[res]) //перебираем данные для удобства, формируя массив данных из объекта с ключами
-    //
-    //                 mess.map((el, i) => {
-    //                     if (el.uid === this.state.user.uid) {
-    //                         this.setState({
-    //                             userCounter: i
-    //                         })
-    //                     }
-    //                 })
-    //
-    //                 const usersCorrectKey = keysOfMessages[this.state.userCounter]
-    //
-    //                 if (usersCorrectKey) {
-    //                     this.setState({
-    //                         usersCorrectKey //id в firebase
-    //                     })
-    //                 }
-    //             }
-    //         })
-    // }
-
     addListeners = async userId => {
 
         await axios.get(`https://chat-14c5a-default-rtdb.europe-west1.firebasedatabase.app/users/${userId}.json`)
